c09f9f9f8df534d2b103689c4459bec6964d6e51
nada <nada.ahmed.elsayed11@gmail.com>
2026-02-04 07:55:00 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,17 @@
+#Task 1: Create a Contact Book
+#1. Create a dictionary called contacts 
+#2. Add three contacts to the dictionary
+contacts = {"nada" : "0123567899" , 
+"sara" : "09876321177" , 
+"lara" : "09876767676"}
+#3. Write a script
+#Write a script to:
+#o Print all the names in the contact book.
+for name in contacts.keys():
+  print (name)
+students = [
+    {"name": "Nada", "grades": [80, 90, 85]},
+    {"name": "Sara", "grades": [70, 75, 80]},
+    {"name": "Lara", "grades": [95, 100, 90]}
+]
+
